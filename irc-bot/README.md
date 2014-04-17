6630acb20f7da33e8144bf0d8d667b8d962efec5
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2014-04-17 08:50:57 +0700

    Make some fun with IRC Bot #1

---
+++ irc-bot/README.md
@@ -0,0 +1,7 @@
+irc-bot
+=====================
+
+Make some fun on IRC
+
+-----------------------------------------------------------
+irc-bot.pl : just connecting to the server, ctrl+c for exit
---
+++ irc-bot/ircbot.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use IO::Socket;
+
+my $user="pff-ircbot";
+my $nick=shift || "pff-ircbot";
+my $server="chat.freenode.net";
+my $port=6667;
+my @channel=qw(kalamangga.net);
+
+print "Connecting to server : $server ";
+my $sock = IO::Socket::INET->new(PeerAddr=>$server,PeerPort=>$port,Proto=>'tcp')
+	or die "[error] $!\n";
+print "[connected]\n";
+$sock->print("USER $nick $user $user $user\n");
+$sock->print("NICK $nick\n");
+for my $channel (@channel) {
+	$sock->print("JOIN #$channel\n");
+};
+while (<$sock>) {
+	chomp;
+	print "$_\n";
+	ping_req($sock, $1) if /^PING\s*:(.*?)$/i;
+};
+close $sock;
+print "Disconnected from $server";
+
