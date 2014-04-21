02f653bab60e3106a18511098c95db68d86eafa1
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2014-04-21 09:38:47 +0700

    ircbot : change default nick

--- irc-bot/ircchanbot.pl
+++ irc-bot/ircchanbot.pl
@@ -6,7 +6,7 @@ use IO::Socket;
 use POSIX qw(strftime);
 
 my $user="pff-icb";
-my $nick=shift || "pff-ircchanbot";
+my $nick=shift || "pff-icb";
 my $server="chat.freenode.net";
 my $port=6667;
 my @channel=qw(kalamangga.net);
@@ -18,7 +18,7 @@ print "Connecting to server : $server ";
 my $sock = IO::Socket::INET->new(PeerAddr=>$server,PeerPort=>$port,Proto=>'tcp')
 	or die "[error] $!\n";
 print "[connected]\n";
-$sock->print("USER $nick $user $user $user\n");
+$sock->print("USER $user $user $user $user\n");
 $sock->print("NICK $nick\n");
 for my $channel (@channel) {
 	$sock->print("JOIN #$channel\n");
